31ff8cbc385a732f77bec2830e968cb4419ac553
camila-cfox <croberto4@alumno.uned.es>
2021-09-06 23:25:10 +0200

    ultimas modificaciones - ! Definite assignment assertion

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,16 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Camila', apellido: 'Roberto', email: 'camilar@gmail.com', createAt: '2021-08-17'},
+  {id: 2, nombre: 'Sergio', apellido: 'Encabo', email: 'sergio.encabo@gmail.com', createAt: '2021-03-31'},
+  {id: 3, nombre: 'Davi', apellido: 'Encabo', email: 'davienc@gmail.com', createAt: '2021-08-27'},
+  {id: 4, nombre: 'Leonardo', apellido: 'Encabo', email: 'leo.enc@gmail.com', createAt: '2021-08-14'},
+  {id: 5, nombre: 'Olga', apellido: 'Alamo', email: 'olga.alamo@gmail.com', createAt: '2021-07-21'},
+  {id: 6, nombre: 'Carlos', apellido: 'Encabo', email: 'carlos.encabo@gmail.com', createAt: '2021-08-17'},
+  {id: 7, nombre: 'Eli', apellido: 'Michalko', email: 'eli.michalko@gmail.com', createAt: '2021-12-25'},
+  {id: 8, nombre: 'Anto', apellido: 'Delgado', email: 'anto.delgado@gmail.com', createAt: '2021-05-02'},
+  {id: 9, nombre: 'Richard', apellido: 'Helm', email: 'richard.helm@gmail.com', createAt: '2017-11-15'},
+  {id: 10, nombre: 'Ralph', apellido: 'Johnson', email: 'ralph.johnson@gmail.com', createAt: '2021-08-13'},
+  {id: 11, nombre: 'Magma', apellido: 'Lee', email: 'magma.lee@gmail.com', createAt: '2021-08-12'},
+  {id: 12, nombre: 'Tornado', apellido: 'Roe', email: 'tornado.roe@gmail.com', createAt: '2021-02-17'}
+];
